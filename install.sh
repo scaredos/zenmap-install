5ae8609b1d2d552a8883bc3b51a51a3dbafffcab
Steven <scared@tuta.io>
2023-02-09 11:15:52 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-sudo apt update && sudo apt upgrade -y
-sudo apt remove nmap -y 
-sudo apt autoremove -y
-sudo apt install unzip python3-pip build-essential checkinstall zlib1g-dev libssl-dev libcurl4-openssl-dev -y
+apt update && sudo apt upgrade -y
+apt remove nmap -y 
+apt autoremove -y
+apt install unzip python3-pip build-essential checkinstall zlib1g-dev libssl-dev libcurl4-openssl-dev -y
 
 pip3 install pycairo
 
@@ -19,12 +19,10 @@ echo 'Make sure u edit lines 60 for nmap and ndiff paths'
 
 sleep 2
 
-nano zenmap/share/zenmap/config/zenmap
+nano zenmap/share/zenmap/config/zenmap.conf
 
 ./configure
 
 make
-
-sudo make install
-
-sudo zenmap
+make install
+zenmap
